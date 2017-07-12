b5a73185d000aa09bb6bf927a5006b08671a4266
Richard Moss <richard.moss@cyotek.com>
2017-07-12 17:42:19 +0100

    Added dual signing
    Release DLL is now dual signed with sha1 for XP and sha256 for anything
    more modern. Also deleted duplicate build file.

--- build.cmd
+++ build.cmd
@@ -6,13 +6,14 @@ CALL ..\..\..\build\set35vars.bat
 
 REM Build and sign the file
 %msbuildexe% Cyotek.Drawing.BitmapFont.sln /p:Configuration=Release /verbosity:minimal /nologo /t:Clean,Build
-CALL signcmd src\Cyotek.Drawing.BitmapFont\bin\Release\Cyotek.Drawing.BitmapFont.dll
+CALL dualsigncmd src\Cyotek.Drawing.BitmapFont\bin\Release\Cyotek.Drawing.BitmapFont.dll
 
 REM Create the package
 PUSHD %CD%
 IF NOT EXIST releases MKDIR releases
 CD releases
-NUGET pack ..\src\Cyotek.Drawing.BitmapFont\Cyotek.Drawing.BitmapFont.csproj -Prop Configuration=Release
+%NUGETexe% pack ..\src\Cyotek.Drawing.BitmapFont\Cyotek.Drawing.BitmapFont.csproj -Prop Configuration=Release
+%zipexe% a -bd -tZip Cyotek.Drawing.BitmapFont.x.x.x.x.zip ..\src\Cyotek.Drawing.BitmapFont\bin\Release\Cyotek.Drawing.BitmapFont.dll
 POPD
 
 ENDLOCAL
