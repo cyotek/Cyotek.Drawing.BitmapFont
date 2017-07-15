2ee321e69d009a58f203a46df3fcbc3b798631a4
Richard Moss <richard.moss@cyotek.com>
2017-07-15 08:38:48 +0100

    Version corrections

--- src/Benchmarks/Properties/AssemblyInfo.cs
+++ src/Benchmarks/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("fb715af4-676a-4202-b7e6-e4ad48e0bb8b")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- tests/Properties/AssemblyInfo.cs
+++ tests/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 [assembly: Guid("cbe1e589-345d-484d-8fda-0004f2780860")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
