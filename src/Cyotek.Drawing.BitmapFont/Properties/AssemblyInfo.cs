4276ae52fdf47794e0be534b67f0b889df979574
Thomas Müller <thomas94@gmx.net>
2017-08-13 21:17:07 +0900

    Do not use automatically generated AssemblyInfo.cs
    This commit prevents automatic generation of AssemblyInfo.cs from project settings by setting the GenerateAssemblyInfo property of BitmapFont to false. Instead, the original AssemblyInfo.cs is directly made use of.

---
+++ src/Cyotek.Drawing.BitmapFont/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cyotek Bitmap Font Parser")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cyotek Ltd")]
+[assembly: AssemblyProduct("Cyotek Bitmap Font Parser")]
+[assembly: AssemblyCopyright("Copyright © 2012-2017 Cyotek Ltd. All Rights Reserved")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("d0f2bfc9-ee27-4fbc-9d0c-c61423b51594")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.3.3.0")]
+[assembly: AssemblyInformationalVersion("1.3.3")]
